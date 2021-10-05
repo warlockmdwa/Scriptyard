8e5b4f44f75d462639b501f64738fcfc1824cc49
warlockmdwa <66335801+warlockmdwa@users.noreply.github.com>
2021-10-05 07:34:08 -0400

    Add files via upload

---
+++ jedAcademy/assets/js/navbar.js
@@ -0,0 +1,20 @@
+
+      
+const mainMenu = document.querySelector('.mainMenu');
+const closeMenu = document.querySelector('.closeMenu');
+const openMenu = document.querySelector('.openMenu');
+
+
+
+openMenu.addEventListener('click',show);
+closeMenu.addEventListener('click',close);
+function show(){
+    mainMenu.style.display = 'flex';
+    mainMenu.style.top = '0';
+
+}
+function close(){
+    mainMenu.style.top = '-100%';
+        mainMenu.style.position = '4254254';
+
+}
